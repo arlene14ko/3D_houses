10f844538e1dffb092d57def5e1d2faaf8cc68a7
Arlene <arlene14ko@gmail.com>
2021-02-25 10:00:29 +0100

    Adding the docstrings and typing

--- utils/api.py
+++ utils/api.py
@@ -1,9 +1,31 @@
+#importing requests library to get the request from the API
 import requests
+from Typing import List, Dict
 
 
 class API:
+    """
+    Class API is where we request the details of the address from the API
+    It contains two functions namely:
+    :get_details function where we check if the address is in the API and get the coordinates
+    :get_polygon function where we get the geometry polygon of the address from the API
+    """
+
         
-    def get_details(address):
+    def get_details(address: List) -> Dict[str,str]:
+        """
+        get_details function requires the address as a parameter and then it will send a request to the API
+            it will save the result as a dictionary named as address_info and return the dictionary address_info
+        :attrib req will get the requests from the api in a json file using the address parameter
+        :attrib address_info contains the result of the request in a dictionary format
+        :attrib address_info includes:
+        :address_info['x_value'] contains the X coordinates in Belgian Lambert
+        :address_info['y_value'] contains the Y coordinates in Belgian Lambert
+        :address_info['street'] contains the street name of the address
+        :address_info['house_number'] contains the house number of the address
+        :address_info['postcode'] contains the postcode of the address
+        :address_info['municipality'] contains the municipality of the address
+        """
         req = requests.get(f"https://loc.geopunt.be/v4/Location?q={address}").json()
         address_info = {'x_value': req['LocationResult'][0]['Location']['X_Lambert72'],
                         'y_value': req['LocationResult'][0]['Location']['Y_Lambert72'],
@@ -12,8 +34,17 @@ class API:
                         'postcode': req['LocationResult'][0]['Zipcode'],
                         'municipality': req['LocationResult'][0]['Municipality']}
         return address_info
-    
-    def get_polygon(address_info):
+
+
+    def get_polygon(address_info: Dict[str,str]) -> Dict[str,str]:
+        """
+        get_polygon function requires the address_info as the parameter and then it will send a request to the API
+            and it will save the result as a dictionary named polygon and return the polygon
+        :attrib req will get the requests from the api in a json file using the address_info parameter
+        :attrib detail will request the gebouwenheden of the address from the API
+        :attrib build will request the gebouw of the address from the APi
+        :attrib polygon will contain the geometripolygon of the address from the API
+        """
         req = requests.get("https://api.basisregisters.vlaanderen.be/v1/adresmatch",
                            params={"postcode": address_info['postcode'],
                                     "straatnaam": address_info['street'],
--- utils/geotiff.py
+++ utils/geotiff.py
@@ -1,3 +1,4 @@
+#importing the necessary libraries
 import pandas as pd
 from io import BytesIO
 from urllib.request import urlopen
@@ -7,9 +8,26 @@ from rasterio.mask import mask
 import shutil
 
 class GeoTiff:
+    """
+    Class GeoTiff  is where we get all the details about the GeoTiff files
+    It has four functions namely:
+    :check_tiff function to check from which tiff file the address is
+    :get_tiff function to download the necessary tiff files
+    :mask_tiff function to create the masked tiff files from the DTM and DSM files
+    :get_chm function to get the CHM of the tiff files
+    """
     
     
-    def check_tiff(x, y):
+    def check_tiff(x: str, y: str) -> str:
+        """
+        check_tiff function is where we check from which tiff file the address belongs to.
+        It needs the x and y coordinates as a parameter
+        :attrib bbox will read the bounding-box.csv which contains the list of all the tiff files
+        It will then go inside a for loop and check if the x and y is inside the bounding box of the tiff file
+        Once it is done, the for loop will break and it will return the attrib i
+        :attrib i will contain from which index the tiff file belongs to
+        :attrib num will contain the number of the tiff file
+        """
         bbox = pd.read_csv('./data/bounding-box.csv')
         for i in range(bbox.shape[0]):
             if bbox['Left (X)'][i] <= x:
@@ -17,7 +35,8 @@ class GeoTiff:
                     if bbox['Bottom (Y)'][i] <= y:
                         if bbox['Top (Y)'][i] >= y:
                             i = i
-                            break                
+                            break
+
         if i < 9:
             num = f'k0{i+1}'
         else:
@@ -25,7 +44,15 @@ class GeoTiff:
         return num
     
     
-    def get_tiff(num):
+    def get_tiff(num: str):
+        """
+        get_tiff function is where it will download the necessary tiff files connected to the address
+        It requires the num from the check_tiff as the parameter and then it will return a print statement
+        :attrib files is a dictionary containing the DSM and DTm with the links on where to download it
+        It will then go inside a for loop and use the urlopen, ZipFile, BytesIO to be able to extract the file without
+        downloading the zip file. It will save the file in the data/temp-raster folder
+        """
+
         files = {'DSM': f"https://downloadagiv.blob.core.windows.net/dhm-vlaanderen-ii-dsm-raster-1m/DHMVIIDSMRAS1m_{num}.zip", 
                  'DTM': f"https://downloadagiv.blob.core.windows.net/dhm-vlaanderen-ii-dtm-raster-1m/DHMVIIDTMRAS1m_{num}.zip"}
         
@@ -40,8 +67,17 @@ class GeoTiff:
         return("Successfully extracted the tiff files!")
     
     
-    def mask_tiff(num, polygon):
-        print("Mask tiff function")
+    def mask_tiff(num: str, polygon: Dict[str,str]) -> Dict[str, str]:
+        """
+        mask_tiff function will create the masked files for the DSM and DTM
+        it requires the num and polygon as the parameter
+        :attrib raster_files is a dictionary containing the path to the tif files
+        :attrib masked_files is a dictionary that will contain the path of the masked files
+        It will go inside a for loop and create the masked files using the rasterio mask module
+        After masking the file, it will save the masked tiff file in the data/masked-files folder
+        Then it will delete the data/temp-raster folder using the shutil module
+        The function will return the masked files in a dictionary format
+        """
         raster_files = {'DSM' : f"./data/temp-raster/DSM/GeoTiff/DHMVIIDSMRAS1m_{num}.tif",
                         'DTM' : f"./data/temp-raster/DTM/GeoTiff/DHMVIIDTMRAS1m_{num}.tif"}
         masked_files = {}
@@ -55,6 +91,7 @@ class GeoTiff:
                          "width": out_img.shape[2],
                          "transform": out_transform,
                          "crs": epsg_code
+                         "developer-name": "Arlene Postrado"
                         })
 
             with rt.open(f"./data/masked-files/{name}_masked.tif", "w", **out_meta) as dest:
@@ -69,7 +106,16 @@ class GeoTiff:
         return masked_files
             
     
-    def get_chm(masked_files):
+    def get_chm(masked_files: Dict[str,str]):
+        """
+        get_chm function will get the CHM from the dsm and dtm
+        It requires the masked_files as the parameter and returns the CHM
+        :attrib dsm_tiff will contain the DSM masked file
+        :attrib dsm_array will read the 1st band of the the dsm_tiff
+        :attrib dtm_tiff will contain the DTM masked file
+        :attrib dtm_array will read the 1st band of the dtm_tiff
+        :attrib chm contains the CHM array
+        """
         dsm_tiff = rt.open(masked_files['DSM'])
         dsm_array = dsm_tiff.read(1)
 
